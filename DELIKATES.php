bf9c4cdbeca8c86bd3aaba4e57c1bcdfa4ad89fa
ucenjejezika <marko.spanovic@gmail.com>
2025-08-28 20:04:23 +0200

    Add files via upload

---
+++ DELIKATES.php
@@ -0,0 +1,28 @@
+<?php
+$array = array(
+    "POLI" => "111",
+    "PECENICA MANGULICA" => "11", 
+    "GRANDPODERO" => "121",
+    "MAKSI CAJNA" => "55",
+    "KULEN TOPOLA" => "45",
+    "POSEBNA"=> "131",
+    "PILECA PRSA U OMOTU DIMLJENA PERUTNINA"=>"123",
+    "GALA"=>"116",
+    "KULEN MAKSI"=>"40",
+    "PAPRIKA U PAVLACI"=>"615",
+    "KRSTAS"=>"511",
+    "GAUDA PREZIDENT"=>"520",
+    "EDAMER PREZIDENT"=>"538",
+    "SOMBORSKA PREZIDENT"=>"598"
+);
+/*
+
+foreach( $array as $key => $value ){
+    echo $key."\t=>\t".$value."\n";
+}
+
+*/
+
+echo $array["POLI"];
+
+?>
